6c7d3614ecf1fb4256171db6b24ce06b0d1f08bd
Taiki Endo <te316e89@gmail.com>
2020-12-01 10:55:01 +0900

    Improve diff assertion test

--- tests/auxiliary/mod.rs
+++ tests/auxiliary/mod.rs
@@ -1,5 +1,6 @@
-use anyhow::Result;
-use std::{fs, process::Command, str};
+#![allow(dead_code, unused_macros)]
+
+use std::{fs, path::Path, process::Command, str};
 use tempfile::Builder;
 
 pub fn trim(s: &str) -> &str {
@@ -11,13 +12,15 @@ pub fn trim(s: &str) -> &str {
     if cnt < 4 { s[cnt..].trim_end() } else { s.trim_end() }
 }
 
-pub fn assert_eq(expected: &str, actual: &str) {
-    (|| -> Result<()> {
+pub fn assert_diff(expected_path: impl AsRef<Path>, actual: impl AsRef<str>) {
+    let actual = actual.as_ref();
+    let manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
+    let expected_path = &manifest_dir.join(expected_path);
+    (|| -> Result<(), Box<dyn std::error::Error>> {
+        let expected = fs::read_to_string(expected_path)?;
         if expected != actual {
-            let outdir = Builder::new().prefix("tests").tempdir()?;
-            let expected_path = &outdir.path().join("expected.txt");
-            let actual_path = &outdir.path().join("actual.txt");
-            fs::write(expected_path, expected)?;
+            let outdir = Builder::new().prefix("assert_diff").tempdir()?;
+            let actual_path = &outdir.path().join(expected_path.file_name().unwrap());
             fs::write(actual_path, actual)?;
             let status = Command::new("git")
                 .args(&["--no-pager", "diff", "--no-index", "--"])
@@ -28,5 +31,5 @@ pub fn assert_eq(expected: &str, actual: &str) {
         }
         Ok(())
     })()
-    .unwrap_or_else(|e| panic!("{:#}", e))
+    .unwrap_or_else(|e| panic!("{}", e))
 }
