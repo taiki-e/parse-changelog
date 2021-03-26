23219bd2ae50ad9e9e780e18df1ae88d1fa56bcd
Taiki Endo <te316e89@gmail.com>
2021-03-27 03:35:00 +0900

    Update assert_diff

--- tests/auxiliary/mod.rs
+++ tests/auxiliary/mod.rs
@@ -5,7 +5,6 @@ mod cli;
 
 use std::{fs, path::Path, process::Command, str};
 
-use anyhow::Result;
 use tempfile::Builder;
 
 #[cfg(feature = "default")]
@@ -25,20 +24,17 @@ pub fn assert_diff(expected_path: impl AsRef<Path>, actual: impl AsRef<str>) {
     let actual = actual.as_ref();
     let manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
     let expected_path = &manifest_dir.join(expected_path);
-    (|| -> Result<()> {
-        let expected = fs::read_to_string(expected_path)?;
-        if expected.trim() != actual.trim() {
-            let outdir = Builder::new().prefix("assert_diff").tempdir()?;
-            let actual_path = &outdir.path().join(expected_path.file_name().unwrap());
-            fs::write(actual_path, actual)?;
-            let status = Command::new("git")
-                .args(&["--no-pager", "diff", "--no-index", "--"])
-                .args(&[expected_path, actual_path])
-                .status()?;
-            assert!(!status.success());
-            panic!("assertion failed");
-        }
-        Ok(())
-    })()
-    .unwrap_or_else(|e| panic!("{:?}", e))
+    let expected = fs::read_to_string(expected_path).unwrap();
+    if expected.trim() != actual.trim() {
+        let outdir = Builder::new().prefix("assert_diff").tempdir().unwrap();
+        let actual_path = &outdir.path().join(expected_path.file_name().unwrap());
+        fs::write(actual_path, actual).unwrap();
+        let status = Command::new("git")
+            .args(&["--no-pager", "diff", "--no-index", "--"])
+            .args(&[expected_path, actual_path])
+            .status()
+            .unwrap();
+        assert!(!status.success());
+        panic!("assertion failed");
+    }
 }
