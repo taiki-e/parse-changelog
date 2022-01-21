5f15e09d90f830193d48ba3e8d06da7d2655a6ce
Taiki Endo <te316e89@gmail.com>
2022-01-22 00:37:16 +0900

    Distribute prebuilt binaries for aarch64 Linux

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,8 @@ Note: In this file, do not use the hard wrap in the middle of a sentence for com
 
 ## [Unreleased]
 
+- Distribute prebuilt binaries for aarch64 Linux (gnu and musl).
+
 ## [0.4.6] - 2022-01-03
 
 ### CLI
--- tests/auxiliary/cli.rs
+++ tests/auxiliary/cli.rs
@@ -6,9 +6,54 @@ use std::{
 
 use anyhow::Context as _;
 use easy_ext::ext;
+use once_cell::sync::Lazy;
+
+static TARGET: Lazy<String> = Lazy::new(|| {
+    let triple = env::consts::ARCH.to_string();
+    if cfg!(target_os = "macos") {
+        triple + "-apple-darwin"
+    } else if cfg!(target_os = "windows") {
+        if cfg!(target_env = "gnu") {
+            triple + "-pc-windows-gnu"
+        } else if cfg!(target_env = "msvc") {
+            triple + "-pc-windows-msvc"
+        } else {
+            unreachable!()
+        }
+    } else if cfg!(target_env = "gnu") {
+        triple + "-unknown-" + env::consts::OS + "-gnu"
+    } else if cfg!(target_env = "musl") {
+        triple + "-unknown-" + env::consts::OS + "-musl"
+    } else {
+        unreachable!()
+    }
+});
+static RUNNER: Lazy<Option<Vec<String>>> = Lazy::new(|| {
+    let runner: Vec<_> =
+        env::var(format!("CARGO_TARGET_{}_RUNNER", TARGET.replace('-', "_").to_ascii_uppercase()))
+            .ok()?
+            .split(' ')
+            .filter(|s| !s.is_empty())
+            .map(str::to_owned)
+            .collect();
+    if runner.is_empty() {
+        None
+    } else {
+        Some(runner)
+    }
+});
 
 pub fn parse_changelog<O: AsRef<OsStr>>(args: impl AsRef<[O]>) -> Command {
-    let mut cmd = Command::new(env!("CARGO_BIN_EXE_parse-changelog"));
+    let bin = env!("CARGO_BIN_EXE_parse-changelog");
+    let mut cmd = match &*RUNNER {
+        Some(runner) => {
+            let mut cmd = Command::new(&runner[0]);
+            cmd.args(&runner[1..]);
+            cmd.arg(bin);
+            cmd
+        }
+        None => Command::new(bin),
+    };
     cmd.current_dir(env!("CARGO_MANIFEST_DIR"));
     cmd.args(args.as_ref());
     cmd
