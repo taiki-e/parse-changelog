926a8b791fa04dc2b494101e0180a5dac942ab3a
Taiki Endo <te316e89@gmail.com>
2022-02-06 10:54:44 +0900

    Update scripts

--- tools/gen.sh
+++ tools/gen.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 IFS=$'\n\t'
+cd "$(dirname "$0")"/..
 
 # Run code generators.
 #
 # USAGE:
 #    ./tools/gen.sh
-
-cd "$(cd "$(dirname "$0")" && pwd)"/..
 
 cargo run --manifest-path tools/codegen/Cargo.toml
