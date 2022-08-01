696c2cd9d5c0a9dfe9750e3c6325b70a48aa47bb
Taiki Endo <te316e89@gmail.com>
2022-08-01 21:21:00 +0900

    Revert "Fix clippy::assertions_on_result_states warning"
    This reverts commit ea0188477c9176037d409c6546ef84964e8ae033.

--- tests/test.rs
+++ tests/test.rs
@@ -111,9 +111,9 @@ fn failure() {
         assert!(parse(changelog).unwrap_err().is_parse());
     }
 
-    Parser::new().prefix_format("").unwrap();
-    Parser::new().prefix_format("  ").unwrap();
-    Parser::new().prefix_format("\t\n").unwrap();
+    assert!(Parser::new().prefix_format("").is_ok());
+    assert!(Parser::new().prefix_format("  ").is_ok());
+    assert!(Parser::new().prefix_format("\t\n").is_ok());
     let e = Parser::new().prefix_format(r"\/").unwrap_err();
     assert!(e.is_format());
     assert!(e.source().is_some());
